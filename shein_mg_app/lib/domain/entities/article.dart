c9413d3e84416357111815b4597d70cf2054eab4
schorio <schorioignace@gmail.com>
2025-03-06 16:02:18 +0300

    creation des models de l'article

---
+++ shein_mg_app/lib/domain/entities/article.dart
@@ -0,0 +1,19 @@
+class Article {
+  final int idArt;
+  final String titreArt;
+  final double prixArt;
+  final double noteArt;
+  final List<String> couleurArt;
+  final List<String> tailleArt;
+  final List<String> comsArt;
+
+  Article({
+    required this.idArt,
+    required this.titreArt,
+    required this.prixArt,
+    required this.noteArt,
+    required this.couleurArt,
+    required this.tailleArt,
+    required this.comsArt,
+  });
+}
