a0c38ea80ebe1b5bca45635f34f0bf2c6e61cbc4
schorio <schorioignace@gmail.com>
2025-03-10 17:25:02 +0300

    fix la taille des block des tailles de l'article

--- shein_mg_app/lib/presentation/widget/taille_widget.dart
+++ shein_mg_app/lib/presentation/widget/taille_widget.dart
@@ -10,35 +10,32 @@ class TailleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: List.generate(article.tailleArt.length, (index) {
-            return Container(
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.only(right: 10),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: MesCouleurs.secondaire.withOpacity(0.3),
-                ),
-                borderRadius: BorderRadius.circular(15),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: List.generate(article.tailleArt.length, (index) {
+          return Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+            margin: const EdgeInsets.only(right: 8),
+            decoration: BoxDecoration(
+              border: Border.all(
                 color: MesCouleurs.secondaire.withOpacity(0.3),
               ),
-              child: Text(
-                article.tailleArt[index],
-                style: TextStyle(
-                  color: couleur,
-                  fontFamily: 'MontSerrat_3',
-                  fontSize: 12,
-                ),
+              borderRadius: BorderRadius.circular(12),
+              color: MesCouleurs.secondaire.withOpacity(0.3),
+            ),
+            child: Text(
+              article.tailleArt[index],
+              style: TextStyle(
+                color: couleur,
+                fontFamily: 'MontSerrat_3',
+                fontSize: 12,
               ),
-            );
-          }),
-        ),
+            ),
+          );
+        }),
       ),
     );
   }
