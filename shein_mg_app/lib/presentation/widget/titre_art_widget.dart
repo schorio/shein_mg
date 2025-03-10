534189fec8daf11109c05326e6cf6de91e0d1615
schorio <schorioignace@gmail.com>
2025-03-10 17:25:31 +0300

    fix les padding du widget titre de l'article

--- shein_mg_app/lib/presentation/widget/titre_art_widget.dart
+++ shein_mg_app/lib/presentation/widget/titre_art_widget.dart
@@ -22,49 +22,40 @@ class TitreArtWidget extends StatelessWidget {
           '${title.substring(0, 14)} ...'; // Limiter à 10 caractères et ajouter des points de suspension
     }
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 5, left: 5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(width: 5),
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: couleur,
-                    fontFamily: 'MontSerrat_2',
-                  ),
-                ),
-                const SizedBox(height: 3),
-                Row(
-                  children: [
-                    Icon(Icons.star, color: couleur, size: 15),
-                    const SizedBox(width: 3),
-                    Text(
-                      article.noteArt.toString(),
-                      style: const TextStyle(
-                        fontSize: 12,
-                        fontFamily: 'MontSerrat_2',
-                        color: MesCouleurs.noir,
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    const Text("By"),
-                    const SizedBox(width: 3),
-                    Text(
-                      "Shein",
-                      style:
-                          TextStyle(color: couleur, fontFamily: 'MontSerrat_2'),
-                    ),
-                  ],
-                ),
-              ],
+          const SizedBox(width: 5),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 30,
+              color: couleur,
+              fontFamily: 'MontSerrat_2',
             ),
+          ),
+          const SizedBox(height: 3),
+          Row(
+            children: [
+              Icon(Icons.star, color: couleur, size: 15),
+              const SizedBox(width: 3),
+              Text(
+                article.noteArt.toString(),
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontFamily: 'MontSerrat_2',
+                  color: MesCouleurs.noir,
+                ),
+              ),
+              const SizedBox(width: 10),
+              const Text("By"),
+              const SizedBox(width: 3),
+              Text(
+                "Shein",
+                style: TextStyle(color: couleur, fontFamily: 'MontSerrat_2'),
+              ),
+            ],
           ),
         ],
       ),
