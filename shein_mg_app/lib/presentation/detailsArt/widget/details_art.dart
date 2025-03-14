719076fb147dbd60da8c0bb2b2ee2b94b762d222
schorio <schorioignace@gmail.com>
2025-03-14 07:16:02 +0300

    creation de page pour afficher les details d'un article

---
+++ shein_mg_app/lib/presentation/detailsArt/widget/details_art.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailsArt extends StatelessWidget {
+  const DetailsArt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
