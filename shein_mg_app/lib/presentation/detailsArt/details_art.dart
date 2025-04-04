d18776b886ff9b7a3fde94f331b1d9274b7e3900
schorio <schorioignace@gmail.com>
2025-04-04 06:34:38 +0300

    DetailsArt : reception de l'argument article

--- shein_mg_app/lib/presentation/detailsArt/details_art.dart
+++ shein_mg_app/lib/presentation/detailsArt/details_art.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:shein_mg_app/domain/entities/article.dart';
 
-class DetailsArt extends StatelessWidget {
+class DetailsArt extends StatefulWidget {
   const DetailsArt({super.key});
 
   @override
+  State<DetailsArt> createState() => _DetailsArtState();
+}
+
+class _DetailsArtState extends State<DetailsArt> {
+  late final article = ModalRoute.of(context)!.settings.arguments as Article;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text(
+          article.titreArt,
+        ),
+      ),
+    );
   }
 }
