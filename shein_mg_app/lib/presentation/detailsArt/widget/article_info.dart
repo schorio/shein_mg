d3319fc33c9f935b20d94f9a435dd4f08edd7d4b
schorio <schorioignace@gmail.com>
2025-04-14 06:51:30 +0300

    ArticleInfo : mise en forme bloc de code en methode

--- shein_mg_app/lib/presentation/detailsArt/widget/article_info.dart
+++ shein_mg_app/lib/presentation/detailsArt/widget/article_info.dart
@@ -30,97 +30,144 @@ class ArticleInfo extends StatelessWidget {
           children: [
             Row(
               children: [
-                Text(
-                  brand.toUpperCase(),
-                  style: const TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
+                fournisseur(),
                 const Spacer(),
-                const Icon(
-                  Icons.star,
-                  color: Colors.orangeAccent,
-                  size: 20,
-                ),
-                const SizedBox(width: 16 / 4),
-                Text(
-                  "$rating ",
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
+                evaluation(context),
               ],
-            ),
-            const SizedBox(height: 5),
-            Text(
-              title,
-              maxLines: 2,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(height: 16),
-            Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(16 / 2),
-                  decoration: BoxDecoration(
-                    color: color,
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(16 / 2),
-                    ),
-                  ),
-                  child: Text(
-                    "Motif",
-                    style: Theme.of(context).textTheme.labelSmall!.copyWith(
-                        color: Colors.white, fontWeight: FontWeight.w500),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Row(
-                  children: List.generate(article.tailleArt.length, (index) {
-                    return Container(
-                      width: 32,
-                      padding: const EdgeInsets.all(5),
-                      margin: const EdgeInsets.only(right: 8),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: MesCouleurs.secondaire.withOpacity(0.3),
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                        color: MesCouleurs.secondaire.withOpacity(0.3),
-                      ),
-                      child: Center(
-                        child: Text(
-                          article.tailleArt[index],
-                          style: TextStyle(
-                            color: color,
-                            fontFamily: 'MontSerrat_3',
-                            fontSize: 12,
-                          ),
-                        ),
-                      ),
-                    );
-                  }),
-                )
-              ],
-            ),
-            const SizedBox(height: 16),
-            Text(
-              "Détails sur l'article",
-              style: Theme.of(context)
-                  .textTheme
-                  .titleMedium!
-                  .copyWith(fontWeight: FontWeight.w500),
             ),
             const SizedBox(height: 2),
-            Text(
-              description,
-              style: const TextStyle(
-                height: 1.4,
-                color: Colors.grey,
-              ),
+            titreArticle(context),
+            const SizedBox(height: 12),
+            Row(
+              children: [
+                motif(context),
+                const SizedBox(width: 10),
+                taille(),
+              ],
             ),
+            const SizedBox(height: 16),
+            details(context),
             const SizedBox(height: 16 / 2),
           ],
         ),
       ),
+    );
+  }
+
+  Column details(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Détails sur l'article",
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium!
+              .copyWith(fontWeight: FontWeight.w500),
+        ),
+        const SizedBox(height: 2),
+        Text(
+          description,
+          style: const TextStyle(
+            height: 1.4,
+            color: Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row taille() {
+    return Row(
+      children: List.generate(article.tailleArt.length, (index) {
+        return Container(
+          width: 32,
+          padding: const EdgeInsets.all(5),
+          margin: const EdgeInsets.only(right: 8),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: MesCouleurs.secondaire.withOpacity(0.3),
+            ),
+            borderRadius: BorderRadius.circular(10),
+            color: MesCouleurs.secondaire.withOpacity(0.3),
+          ),
+          child: Center(
+            child: Text(
+              article.tailleArt[index],
+              style: TextStyle(
+                color: color,
+                fontFamily: 'MontSerrat_3',
+                fontSize: 12,
+              ),
+            ),
+          ),
+        );
+      }),
+    );
+  }
+
+  Container motif(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16 / 2),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(16 / 2),
+        ),
+      ),
+      child: Text(
+        "Motif",
+        style: Theme.of(context)
+            .textTheme
+            .labelSmall!
+            .copyWith(color: Colors.white, fontWeight: FontWeight.w500),
+      ),
+    );
+  }
+
+  Text titreArticle(BuildContext context) {
+    return Text(
+      title,
+      maxLines: 2,
+      style: Theme.of(context).textTheme.titleLarge,
+    );
+  }
+
+  Row evaluation(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(
+          Icons.star,
+          color: Colors.orangeAccent,
+          size: 20,
+        ),
+        const SizedBox(width: 16 / 4),
+        Text(
+          "$rating ",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ],
+    );
+  }
+
+  Row fournisseur() {
+    return Row(
+      children: [
+        const Text(
+          "By",
+          style: TextStyle(
+            color: Colors.grey,
+          ),
+        ),
+        const SizedBox(width: 3),
+        Text(
+          article.fournisseur.toUpperCase(),
+          style: TextStyle(
+            color: color,
+          ),
+        ),
+      ],
     );
   }
 }
