bf8ecb85cf2edc45bfd21fe7b2a364ec162822aa
schorio <schorioignace@gmail.com>
2025-04-18 07:19:20 +0300

    ImageViewer : utilisation du widget IndexImage

--- shein_mg_app/lib/presentation/detailsArt/widget/image_viewer.dart
+++ shein_mg_app/lib/presentation/detailsArt/widget/image_viewer.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:shein_mg_app/domain/entities/article.dart';
+import 'package:shein_mg_app/presentation/detailsArt/widget/index_image.dart';
 
 class ImageViewer extends StatefulWidget {
   const ImageViewer({super.key});
@@ -18,37 +19,52 @@ class _ImageViewerState extends State<ImageViewer> {
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider.builder(
-      itemCount: article.imageArt.length,
-      options: CarouselOptions(
-        height: MediaQuery.of(context).size.height * 0.75,
-        initialPage: showedIndex,
-        viewportFraction: MediaQuery.of(context).size.width / 380,
-        onPageChanged: (index, reason) {
-          setState(() {
-            showedIndex = index;
-          });
-        },
-      ),
-      itemBuilder: (context, index, _) {
-        return GestureDetector(
-          onDoubleTap: handleDoubleTap,
-          onDoubleTapDown: handleDoubleTapDown,
-          child: InteractiveViewer(
-            transformationController: transController,
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    article.imageArt[index],
-                  ),
-                  fit: BoxFit.cover,
-                ),
-              ),
+    return Stack(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(
+              vertical: MediaQuery.of(context).size.height * 0.1),
+          child: CarouselSlider.builder(
+            itemCount: article.imageArt.length,
+            options: CarouselOptions(
+              height: MediaQuery.of(context).size.height * 0.75,
+              initialPage: showedIndex,
+              viewportFraction: MediaQuery.of(context).size.width / 380,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  showedIndex = index;
+                });
+              },
             ),
+            itemBuilder: (context, index, _) {
+              return GestureDetector(
+                onDoubleTap: handleDoubleTap,
+                onDoubleTapDown: handleDoubleTapDown,
+                child: InteractiveViewer(
+                  transformationController: transController,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          article.imageArt[index],
+                        ),
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
           ),
-        );
-      },
+        ),
+        IndexImage(
+          article: article,
+          index: showedIndex,
+          height: 20,
+          bottom: 0.2,
+          right: 0.43,
+        )
+      ],
     );
   }
 
